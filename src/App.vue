656df0006acc83b6da4d0fbc3e01e56a297d1f5c
ChristineWasike <christinewasike22@gmail.com>
2019-05-16 21:39:57 +0300

    Added my fisrt vue components - alerts

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,24 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
+        <span>Vue</span>
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
+      <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+        <span class="mr-2">Open me</span>
         <v-icon>open_in_new</v-icon>
       </v-btn>
     </v-toolbar>
 
+    <div>
+      <!-- Success Alert -->
+      <v-alert :value="true" type="success">This is a success alert.</v-alert>
+      <!-- Info Alert -->
+      <v-alert :value="true" type="info">This is a info alert.</v-alert>
+
+       <v-alert :value="true" type="success">This is a success alert.</v-alert>
+    </div>
     <v-content>
       <HelloWorld/>
     </v-content>
@@ -23,17 +27,17 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
   },
-  data () {
+  data() {
     return {
       //
-    }
+    };
   }
-}
+};
 </script>
